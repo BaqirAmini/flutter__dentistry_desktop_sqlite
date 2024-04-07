effddbd3837230f2439f49502e22fa87f2f88120
Engineer Baqir Amini <baqir.amini01@gmail.com>
2024-04-07 11:46:07 +0430

    Call the database related function here and some other changes.

--- lib/views/main/splashscreen.dart
+++ lib/views/main/splashscreen.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dentistry/config/developer_options.dart';
 import 'package:flutter_dentistry/config/global_usage.dart';
 import 'package:flutter_dentistry/config/language_provider.dart';
 import 'package:flutter_dentistry/config/liscense_verification.dart';
+import 'package:flutter_dentistry/models/db_conn.dart';
 import 'package:flutter_dentistry/views/main/login.dart';
 import 'package:provider/provider.dart';
 
@@ -50,7 +54,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     // Navigate to the login page after 3 seconds
     Future.delayed(const Duration(seconds: 3), () async {
-   /*    _globalUsage.deleteValue4User('UserlicenseKey');
+      /*    _globalUsage.deleteValue4User('UserlicenseKey');
       _globalUsage.deleteExpiryDate(); */
       await _globalUsage.hasLicenseKeyExpired() ||
               await _globalUsage.getLicenseKey4User() == null
@@ -62,6 +66,43 @@ class _SplashScreenState extends State<SplashScreen> {
           // ignore: use_build_context_synchronously
           : Navigator.pushReplacement(
               context, MaterialPageRoute(builder: (context) => const Login()));
+      try {
+        // Call to create database and tables
+        var conn = await onConnToSqliteDb();
+        var query1 = await conn.rawQuery(
+            'SELECT * FROM staff_auth WHERE username = ?', ['admin123']);
+        if (query1.isEmpty) {
+          int resultStaff = await conn.rawInsert(
+              'INSERT INTO staff (firstname, lastname, phone, family_phone1) VALUES (?, ?, ?, ?)',
+              ['Ahmad', 'Ahmadi', '1234567890', '1234567891']);
+          if (resultStaff > 0) {
+            // Select to see if it exsits.
+            var selectStaff = await conn.rawQuery(
+                'SELECT staff_ID FROM staff WHERE firstname = ? AND lastname = ? AND phone = ?',
+                ['Ahmad', 'Ahmadi', '1234567890']);
+            int staffId = selectStaff.first['staff_ID'] as int;
+
+            //  Do some hashing for password
+            var bytes = utf8.encode('admin123');
+            var digest = sha256.convert(bytes);
+            String hashedPwd = digest.toString();
+          
+            int resultAuth = await conn.rawInsert(
+                'INSERT INTO staff_auth (staff_ID, username, password, role) VALUES (?, ?, ?, ?)',
+                [staffId, 'admin123', hashedPwd, 'مدیر سیستم']);
+
+            if (resultAuth > 0) {
+              print('User ID = $staffId created.');
+            } else {
+              print('Oops! User ID = $staffId not created!');
+            }
+          } else {
+            print('Staff not created.');
+          }
+        }
+      } catch (e) {
+        print('Exception in splashscreen: $e');
+      }
     });
   }
 
